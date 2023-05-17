cc9dce3fdb2de6e5090fb36ce0827c3f27a68273
Denise-Dichiara <dichiara.d@gmail.com>
2023-05-17 17:12:09 +0200

    added message for user and machine

--- myscript.js
+++ myscript.js
@@ -9,7 +9,9 @@ let pcNumber = Math.floor(Math.random() * 7);
 for(i = 0; i < 7; i ++){
     if( userNumber > pcNumber ){
         console.log('Humans rules');
+        document.getElementById('human').innerHTML = 'GO HUMAN'
     }else if( pcNumber > userNumber){
         console.log('AI won')
+        document.getElementById('machine').innerHTML = 'MACHINE WON'
     }
 }
