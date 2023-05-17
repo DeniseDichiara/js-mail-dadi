bb6a8821b7ca94481b0c6ba3068f568932da6c31
Denise-Dichiara <dichiara.d@gmail.com>
2023-05-17 16:42:23 +0200

    created condition for user or pc

--- myscript.js
+++ myscript.js
@@ -1,5 +1,15 @@
 
 //? creare due variabili
 
-let user = Math.floor(Math.random() * 7);
-let pc = Math.floor(Math.random() * 7);
+let userNumber = Math.floor(Math.random() * 7);
+let pcNumber = Math.floor(Math.random() * 7);
+
+//** creare il generatore di numeri
+
+for(i = 0; i < 7; i ++){
+    if( userNumber > pcNumber ){
+        console.log('Humans rules');
+    }else if( pcNumber > userNumber){
+        console.log('AI won')
+    }
+}
